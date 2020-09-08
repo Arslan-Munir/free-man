9533a58935e6e365f2f1212887e4ae5ce687c200
Arslan Munir <Arslan Munir>
2020-09-08 14:51:44 +0500

    Show current location on maps

--- src/app/core/components/sign-in/sign-in.component.ts
+++ src/app/core/components/sign-in/sign-in.component.ts
@@ -1,70 +1,67 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { MapView, Marker, Position } from "nativescript-google-maps-sdk";
 import { registerElement } from "@nativescript/angular/element-registry";
+import * as geolocation from "nativescript-geolocation";
+import { Accuracy } from "tns-core-modules/ui/enums";
 
 registerElement("MapView", () => MapView);
+
 @Component({
     selector: "sign-in",
     templateUrl: "./sign-in.component.html"
 })
 export class SignInComponent {
-    latitude =  0;
-    longitude = 0;
-    zoom = 8;
+    latitude = 33.6844;
+    longitude = 73.0479;
+    zoom = 20;
     minZoom = 0;
     maxZoom = 22;
     bearing = 0;
     tilt = 0;
     padding = [40, 40, 40, 40];
     mapView: MapView;
-
-    lastCamera: string;
+    finalLatitude = 0; finalLongitude = 0;
 
     constructor() {
-        this.latitude =  33.6844;
-        this.longitude = 73.0479;
+        this.getCurrentLocation();
     }
 
-    // Map events
     onMapReady(event) {
-        console.log("Map Ready");
-
         this.mapView = event.object;
-
-        console.log("Setting a marker...");
-
         const marker = new Marker();
-        marker.position = Position.positionFromLatLng(-33.86, 151.20);
-        marker.title = "Sydney";
-        marker.snippet = "Australia";
+        marker.position = Position.positionFromLatLng(this.latitude, this.longitude);
         marker.userData = {index: 1};
         this.mapView.addMarker(marker);
-    }
-
-    onCoordinateTapped(args) {
-        console.log("Coordinate Tapped, Lat: " + args.position.latitude + ", Lon: " + args.position.longitude, args);
-    }
-
-    onMarkerEvent(args) {
-        console.log("Marker Event: '" + args.eventName
-            + "' triggered on: " + args.marker.title
-            + ", Lat: " + args.marker.position.latitude + ", Lon: " + args.marker.position.longitude, args);
-    }
-
-    onCameraChanged(args) {
-        console.log("Camera changed: " + JSON.stringify(args.camera), JSON.stringify(args.camera) === this.lastCamera);
-        this.lastCamera = JSON.stringify(args.camera);
-
     }
 
     onCameraMove(args) {
-        console.log("Camera moving: " + JSON.stringify(args.camera));
         const marker = new Marker();
         marker.position = Position.positionFromLatLng(args.camera.latitude, args.camera.longitude);
-        marker.title = "Sydney";
-        marker.snippet = "Australia";
+        this.finalLatitude = args.camera.latitude;
+        this.finalLongitude = args.camera.longitude;
         marker.userData = {index: 1};
         this.mapView.clear();
         this.mapView.addMarker(marker);
+    }
+
+    show() {
+        alert(this.finalLatitude + "......." + this.finalLongitude);
+    }
+
+    private getCurrentLocation() {
+        geolocation.getCurrentLocation({
+            desiredAccuracy: Accuracy.high,
+            maximumAge: 5000,
+            timeout: 10000
+        }).then((loc) => {
+            if (loc) {
+                this.latitude = loc.latitude;
+                this.longitude = loc.longitude;
+                this.finalLatitude = loc.latitude;
+                this.finalLongitude = loc.longitude;
+            }
+        }, (e) => {
+            console.log("Error: " + (e.message || e));
+        });
     }
 }
